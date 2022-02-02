29ad78711873638ca72ef8c8b53862efff2dc1d7
Sean Finegan <sean.j.finegan@gmail.com>
2022-02-01 20:11:52 -0800

    Added transitions to projects page and added links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import HomePage from "./Views/HomePage/HomePage";
 import ProjectsPage from "./Views/ProjectsPage/ProjectsPage";
 import "./App.scss";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function App() {
+    let location = useLocation();
+
     return (
         <div className="App">
             <main>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="projectsPage" element={<ProjectsPage />} />
-                    {/* <Route path="contact" element={<Contact />} />
+                <TransitionGroup>
+                    <CSSTransition
+                        key={location.pathname}
+                        classNames="page-transition"
+                        timeout={2000}
+                    >
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route
+                                path="projectsPage"
+                                element={<ProjectsPage />}
+                            />
+                            {/* <Route path="contact" element={<Contact />} />
                     <Route path="resume" element={<Resume />} />
                     <Route path="skills" element={<Skills />} />
                     <Route path="about" element={<About />} /> */}
-                </Routes>
+                        </Routes>
+                    </CSSTransition>
+                </TransitionGroup>
             </main>
         </div>
     );
--- src/Views/ProjectsPage/ProjectsPage.js
+++ src/Views/ProjectsPage/ProjectsPage.js
@@ -1,12 +1,33 @@
 import React from "react";
 import Card from "../../components/Card/Card";
+import { CSSTransition } from "react-transition-group";
+
+import "./ProjectPage.scss";
+const data = require("./projects.json");
 
 function ProjectsPage() {
+    console.log(data);
     return (
         <React.Fragment>
-            <div className="d-flex flex-wrap flex-md-nowrap justify-content-center">
-                <Card image={"peClipboardMobile.png"} />
-                <Card image={"sonicOne copy.png"} />
+            <div className="d-flex full-page align-items-center">
+                <div className="d-flex flex-wrap justify-content-center">
+                    {data.map((project, i) => {
+                        return (
+                            <CSSTransition
+                                key={project + i}
+                                classNames="page-transition"
+                                timeout={1000}
+                            >
+                                <Card
+                                    title={project.title}
+                                    image={project.image}
+                                    description={project.description}
+                                    link={project.link}
+                                />
+                            </CSSTransition>
+                        );
+                    })}
+                </div>
             </div>
         </React.Fragment>
     );
