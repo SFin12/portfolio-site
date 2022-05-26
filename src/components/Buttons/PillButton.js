50d43d73bc1b29299f530278c596728cbdd4092b
Sean Finegan <73453120+SFin12@users.noreply.github.com>
2022-05-25 21:21:49 -0700

    Delete src directory

--- src/components/Buttons/PillButton.js
+++
@@ -1,8 +0,0 @@
-import { HashLink } from "react-router-hash-link";
-import "./PillButton.scss";
-
-export const PillButton = ({ name, to, close }) => (
-    <HashLink smooth to={`/#${to}`} onClick={close}>
-        <button className="pill-button">{name}</button>
-    </HashLink>
-);
